8a155f16dbc9b9aa22fae2e338a81741e1d550db
renato <renato_developer@hotmail.com>
2018-05-26 22:47:04 -0300

    inicialização do contexto mvp para login.

--- app/src/main/java/com/developer/bestbuy/application/service/login/LoginService.java
+++ app/src/main/java/com/developer/bestbuy/application/service/login/LoginService.java
@@ -1,4 +1,27 @@
 package com.developer.bestbuy.application.service.login;
 
+import android.content.Context;
+
+import com.developer.bestbuy.application.service.ILoginView;
+
 public class LoginService {
+    private Context context;
+    private ILoginView view;
+
+    public LoginService(Context context, ILoginView view){
+        this.context = context;
+        this.view = view;
+    }
+
+    public boolean login(){
+        try{
+            if(this.view.getUsername().length()> 0 && this.view.getPassword().length()>0){
+                return  true;
+            }
+        }catch (Exception e){
+            e.getMessage().toString();
+        }
+        return  false;
+    }
+
 }
